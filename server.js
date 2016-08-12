31c81afe43c7601e567c3fa39877bd7cdef1a278
Usman Ali <usmanjjjj@gmail.com>
2016-08-12 16:19:42 +0500

    Initialize repo

---
+++ server.js
@@ -0,0 +1,22 @@
+var portNumber = 3000;
+var express = require('express');
+	var app = express();
+
+// app.get('/', function(req, res){
+// 	res.send("hello usman");
+// });
+var middleware = require('./middleware.js');
+
+app.use(middleware.logger);
+
+app.get('/about', middleware.requireAuthentication, function(req, res){
+	res.send('it is about page');
+});
+
+app.use(express.static(__dirname + '/public'));
+
+
+app.listen(portNumber, function(){
+	console.log("server is startd");
+	console.log('at port: '+portNumber);
+});
